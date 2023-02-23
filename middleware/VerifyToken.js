2d02cfc6b6f390a36e931f1d8878324d9431c142
Sameeulla Mujawar <124555542+SameeullaMujawar@users.noreply.github.com>
2023-02-23 15:32:05 +0530

    Update VerifyToken.js

--- middleware/VerifyToken.js
+++ middleware/VerifyToken.js
@@ -1,5 +1,4 @@
 import jwt from "jsonwebtoken";
- 
 export const verifyToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
